528052b7d829d0bc1bf865dab007ba074aa604bb
YashSYeola <yashsy7803@gmail.com>
2024-02-01 19:07:43 +0530

    Initial commit msg

---
+++ js/music-list.js
@@ -0,0 +1,23 @@
+
+let allMusic = [
+    {
+      name: "Salaam Rocky Bai",
+      artist: "Yash",
+      img: "music-1",
+      src: "music-1"
+    },
+    {
+      name: "Animal Ringtone",
+      artist: "Arjan Vailly",
+      img: "music-2",
+      src: "music-2"
+    },
+    {
+      name: "Ishq Instrumental",
+      artist: "Guru Randhawa",
+      img: "music-3",
+      src: "music-3"
+    },
+
+
+  ];
